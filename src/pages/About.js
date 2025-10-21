0b7a243dd28172b4aec17b5a43fa15744fe8a93e
unknown <al_bacs@yahoo.com>
2025-10-21 10:18:59 +0800

    adding particles

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,6 +13,7 @@ import Ionic from "../images/ionic-icon-svgrepo-com.svg";
 import PPT from "../images/ppt.png";
 import MSWORD from "../images/word.png";
 import EXCEL from "../images/excel.png";
+import VSCODE from "../images/vscode-svgrepo-com.svg";
 
 AOS.init();
 
@@ -46,166 +47,98 @@ const About = () => {
               </p>
               <h5 className="text-center skills-about">EXPERIENCE</h5>
 
-              <ul className="about-list">
-                <li
-                  className="list"
-                  id="ht"
-                  data-bs-toggle="popover"
-                  data-bs-placement="top"
-                  data-bs-content="Top popover"
-                  title="HTML"
-                >
-                  <i class="fa-brands fa-html5 fa-html5 fa-bounce fa-2xl"></i>
-                </li>
-                <li
-                  className="list"
-                  id="cs"
-                  data-bs-toggle="tooltip"
-                  data-bs-placement="top"
-                  title="CSS"
-                >
-                  <i class="fa-brands fa-css3-alt fa-flip fa-2xl"></i>
-                </li>
-                <li
-                  className="list"
-                  id="bo"
-                  data-bs-toggle="tooltip"
-                  data-bs-placement="top"
-                  title="BOOTSTRAP"
-                >
-                  <i class="fa-brands fa-bootstrap fa-beat-fade fa-2xl"></i>
-                </li>
-                <li
-                  className="list"
-                  id="ja"
-                  data-bs-toggle="tooltip"
-                  data-bs-placement="top"
-                  title="JAVASCRIPT"
-                >
-                  <i class="fa-brands fa-square-js fa-shake fa-2xl"></i>
-                </li>
-                <li
-                  className="list"
-                  id="re"
-                  data-bs-toggle="tooltip"
-                  data-bs-placement="top"
-                  title="REACT"
-                >
-                  <i class="fa-brands fa-react fa-spin-pulse fa-2xl"></i>
-                </li>
-                <li
-                  className="list"
-                  id="gi"
-                  data-bs-toggle="tooltip"
-                  data-bs-placement="top"
-                  title="GITHUB"
-                >
-                  <i class="fa-brands fa-github fa-fade fa-2xl"></i>
-                </li>
-                <li
-                  className="list"
-                  id="py"
-                  data-bs-toggle="tooltip"
-                  data-bs-placement="top"
-                  title="PYTHON"
-                >
-                  <i className="fa-brands fa-python fa-spin fa-2xl"></i>
-                </li>
-                <li
-                  className="list"
-                  id="my"
-                  data-bs-toggle="tooltip"
-                  data-bs-placement="top"
-                  title="SQL"
-                >
-                  MYSQL
-                </li>
-                <li
-                  className="list"
-                  id="ph"
-                  data-bs-toggle="tooltip"
-                  data-bs-placement="top"
-                  title="PHP"
-                >
-                  <i class="fa-brands fa-php fa-beat-fade fa-2xl"></i>
-                </li>
-                <li
-                  className="list"
-                  id="photo"
-                  data-bs-toggle="tooltip"
-                  data-bs-placement="top"
-                  title="PHOTOSHOP"
-                >
-                  <img src={Photoshop} className="img-fluid" alt="..." />
-                </li>
-                <li
-                  className="list"
-                  id="fil"
-                  data-bs-toggle="tooltip"
-                  data-bs-placement="top"
-                  title="FILMORA"
-                >
-                  <img src={Filmora} className="img-fluid" alt="filmora logo" />
-                </li>
-                <li
-                  className="list"
-                  id="tail"
-                  data-bs-toggle="tooltip"
-                  data-bs-placement="top"
-                  title="TAILWIND"
-                >
-                  <img
-                    src={Tailwind}
-                    className="img-fluid"
-                    alt="tailwindlogo"
-                  />
-                </li>
-                <li
-                  className="list"
-                  id="fig"
-                  data-bs-toggle="tooltip"
-                  data-bs-placement="top"
-                  title="FIGMA"
-                >
-                  <img src={Figma} className="img-fluid" alt="figmalogo" />
-                </li>
-                <li
-                  className="list"
-                  id="fig"
-                  data-bs-toggle="tooltip"
-                  data-bs-placement="top"
-                  title="IONIC"
-                >
-                  <img src={Ionic} className="img-fluid" alt="figmalogo" />
-                </li>
-                 <li
-                  className="list"
-                  id="ms"
-                  data-bs-toggle="tooltip"
-                  data-bs-placement="top"
-                  title="MSWORD"
-                >
-                  <img src={MSWORD} className="img-fluid" alt="figmalogo" />
-                </li>
-                 <li
-                  className="list"
-                  id="ms"
-                  data-bs-toggle="tooltip"
-                  data-bs-placement="top"
-                  title="EXCEL"
-                >
-                  <img src={EXCEL} className="img-fluid" alt="figmalogo" />
-                </li>
-                 <li
-                  className="list"
-                  id="ms"
-                  data-bs-toggle="tooltip"
-                  data-bs-placement="top"
-                  title="POWER POINT"
-                >
-                  <img src={PPT} className="img-fluid" alt="figmalogo" />
-                </li>
-              </ul>
+              <ul className="about-list text-center">
+  <li className="list" title="HTML" style={{backgroundColor:"#0d6cf9ff"}}>
+    <i className="fa-brands fa-html5 fa-bounce fa-2xl"></i>
+    <span className="d-block mt-2 text-muted" style={{ fontSize: "0.75rem"}}>HTML</span>
+  </li>
+
+  <li className="list" title="CSS" style={{backgroundColor:"#f17508ff"}}>
+    <i className="fa-brands fa-css3-alt fa-flip fa-2xl"></i>
+    <span className="d-block mt-2 text-muted" style={{ fontSize: "0.75rem" }}>CSS</span>
+  </li>
+
+  <li className="list" title="BOOTSTRAP" style={{backgroundColor:"#8e09f3ff"}}>
+    <i className="fa-brands fa-bootstrap fa-beat-fade fa-2xl"></i>
+    <span className="d-block mt-2 text-muted" style={{ fontSize: "0.75rem" }}>BOOTSTRAP</span>
+  </li>
+
+  <li className="list" title="JAVASCRIPT" style={{backgroundColor:"#f0f408ff"}}>
+    <i className="fa-brands fa-square-js fa-shake fa-2xl"></i>
+    <span className="d-block mt-2 text-muted" style={{ fontSize: "0.75rem" }}>JAVASCRIPT</span>
+  </li>
+
+  <li className="list" title="REACT"style={{backgroundColor:"#0de9f9ff"}}>
+    <i className="fa-brands fa-react fa-spin-pulse fa-2xl"></i>
+    <span className="d-block mt-2 text-muted" style={{ fontSize: "0.75rem" }}>REACT</span>
+  </li>
+
+  <li className="list" title="GITHUB" style={{backgroundColor:"#c5f0f4ff"}}>
+    <i className="fa-brands fa-github fa-fade fa-2xl"></i>
+    <span className="d-block mt-2 text-muted" style={{ fontSize: "0.75rem" }}>GITHUB</span>
+  </li>
+
+  <li className="list" title="PYTHON" style={{backgroundColor:"#caf90dff"}}>
+    <i className="fa-brands fa-python fa-spin fa-2xl"></i>
+    <span className="d-block mt-2 text-muted" style={{ fontSize: "0.75rem" }}>PYTHON</span>
+  </li>
+
+  <li className="list" title="MYSQL">
+    <i className="fa-solid fa-database fa-beat-fade fa-2xl"></i>
+    <span className="d-block mt-2 text-muted" style={{ fontSize: "0.75rem" }}>MYSQL</span>
+  </li>
+
+  <li className="list" title="PHP">
+    <i className="fa-brands fa-php fa-beat-fade fa-2xl"></i>
+    <span className="d-block mt-2 text-muted" style={{ fontSize: "0.75rem" }}>PHP</span>
+  </li>
+
+  <li className="list" title="PHOTOSHOP">
+    <img src={Photoshop} className="img-fluid" alt="photoshop logo" />
+    <span className="d-block mt-2 text-muted" style={{ fontSize: "0.75rem" }}>PHOTOSHOP</span>
+  </li>
+
+  <li className="list" title="FILMORA">
+    <img src={Filmora} className="img-fluid" alt="filmora logo" />
+    <span className="d-block mt-2 text-muted" style={{ fontSize: "0.75rem" }}>FILMORA</span>
+  </li>
+
+  <li className="list" title="TAILWIND">
+    <img src={Tailwind} className="img-fluid" alt="tailwind logo" />
+    <span className="d-block mt-2 text-muted" style={{ fontSize: "0.75rem" }}>TAILWIND</span>
+  </li>
+
+  <li className="list" title="FIGMA">
+    <img src={Figma} className="img-fluid" alt="figma logo" />
+    <span className="d-block mt-2 text-muted" style={{ fontSize: "0.75rem" }}>FIGMA</span>
+  </li>
+
+  <li className="list" title="IONIC">
+    <img src={Ionic} className="img-fluid" alt="ionic logo" />
+    <span className="d-block mt-2 text-muted" style={{ fontSize: "0.75rem" }}>IONIC</span>
+  </li>
+
+  <li className="list" title="MS WORD">
+    <img src={MSWORD} className="img-fluid" alt="word logo" />
+    <span className="d-block mt-2 text-muted" style={{ fontSize: "0.75rem" }}>MS WORD</span>
+  </li>
+
+  <li className="list" title="EXCEL">
+    <img src={EXCEL} className="img-fluid" alt="excel logo" />
+    <span className="d-block mt-2 text-muted" style={{ fontSize: "0.75rem" }}>EXCEL</span>
+  </li>
+
+  <li className="list" title="POWERPOINT">
+    <img src={PPT} className="img-fluid" alt="powerpoint logo" />
+    <span className="d-block mt-2 text-muted" style={{ fontSize: "0.75rem" }}>PPT</span>
+  </li>
+
+  <li className="list" title="VSCODE">
+    <img src={VSCODE} className="img-fluid" alt="vscode logo" />
+    <span className="d-block mt-2 text-muted" style={{ fontSize: "0.75rem" }}>VSCODE</span>
+  </li>
+</ul>
+
 
               <p className="card-text">
                 <small className="text-muted"></small>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,57 +1,90 @@
-
 import { Tabtitle } from '../components/GeneralFunctions';
 import React, { useEffect } from 'react';
 import Typewriter from "typewriter-effect";
-import  Carousel  from '../components/Carousel';
+import Carousel from '../components/Carousel';
 import "../css/home.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
+// Particles
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
 
-AOS.init();
-
-
-
-const Home = ()=> {
-    
+const Home = () => {
 
     Tabtitle('Home | JLB');
+
     useEffect(() => {
-      window.scrollTo(0, 0);
+        window.scrollTo(0, 0);
+        AOS.init({ duration: 1000 });
     }, []);
-    return(
-  
-    <div className="container mb-5 " id='home-container'> 
-  
-  
-    <div className="row text-center mt-5 mb-5 hero-section"> 
-        <div className="col-sm-6 hero-text mb-3">
-        <div  className='hello'data-aos="fade-right" data-aos-duration="1000"> Hello my name is</div>
-         <div className="myname" data-aos="fade-up" data-aos-duration="1000">Joseph Lester Bacsarsa</div>
-       
-        <div className="type" data-aos="fade-left" data-aos-duration="1000" >
-        <Typewriter 
 
-            id = "type-text"  
-            className="typewriter-text"
-            options={{
-            strings: ['A Problem Solver','A Front-end Developer','A Web Designer'],
-            autoStart: true,
-             loop: true,
-              }}
-            
-              /> 
-              </div>
-              </div>
+    const particlesInit = async (main) => {
+        await loadFull(main);
+    };
 
-        <div className="col-sm-6 mb-3 carousel-home">{<Carousel/>}</div>
-    </div>
+    const particlesLoaded = (container) => {
+        // optional: console.log(container)
+    };
 
-    </div>  
+    return (
+        <div className="home-wrapper">
+            {/* Particle Background */}
+            <Particles
+                id="tsparticles"
+                init={particlesInit}
+                loaded={particlesLoaded}
+                options={{
+                    background: {
+                        color: { value: "#ffffff" }, // page background
+                    },
+                    fpsLimit: 60,
+                    interactivity: {
+                        events: {
+                            onHover: { enable: true, mode: "repulse" },
+                            onClick: { enable: true, mode: "push" },
+                        },
+                        modes: {
+                            push: { quantity: 4 },
+                            repulse: { distance: 100, duration: 0.4 },
+                        },
+                    },
+                    particles: {
+                        color: { value: "#0B8CD5" },
+                        links: { enable: true, color: "#0B8CD5", distance: 150, opacity: 0.4, width: 1 },
+                        move: { enable: true, speed: 2, outModes: "bounce" },
+                        number: { value: 50, density: { enable: true, area: 800 } },
+                        opacity: { value: 0.5 },
+                        shape: { type: "circle" },
+                        size: { value: { min: 2, max: 5 } },
+                    },
+                    detectRetina: true,
+                }}
+            />
 
-
-
-    )
-    }
+            {/* Page Content */}
+            <div className="container mb-5" id='home-container'>
+                <div className="row text-center mt-5 mb-5 hero-section">
+                    <div className="col-sm-6 hero-text mb-3">
+                        <div className='hello' data-aos="fade-right">Hello my name is</div>
+                        <div className="myname" data-aos="fade-up">Joseph Lester Bacsarsa</div>
+                        <div className="type" data-aos="fade-left">
+                            <Typewriter
+                                id="type-text"
+                                className="typewriter-text"
+                                options={{
+                                    strings: ['A Problem Solver', 'A Front-end Developer', 'A Web Designer'],
+                                    autoStart: true,
+                                    loop: true,
+                                }}
+                            />
+                        </div>
+                    </div>
+                    <div className="col-sm-6 mb-3 carousel-home"><Carousel /></div>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default Home;
